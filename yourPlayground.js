d38efb870db5a4794a93178cae3854d586896d7b
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2022-07-29 12:32:29 -0400

    Getting Started

---
+++ yourPlayground.js
@@ -0,0 +1,41 @@
+console.log("Welcome World");
+
+// *Variables
+uname = "Peter Pan";
+console.log(uname);
+
+sentence = "How are you doing today";
+
+// operators
+
+food = Number(prompt("How much was your food?"));
+tipPercentage = Number(prompt("tip %") / 100);
+tipAmount = food * tipPercentage;
+total = tipPercentage + tipAmount;
+console.log("tip:", tipAmount);
+console.log("total", total);
+
+// * user input
+
+/** //*data types (string, numbers)
+ * *numbers 👉🏾 1,5,10,100,2.5
+ * *strings 👉🏾 'hello', "What is up"
+ * *arrays 👉🏾 []
+ * *objects 👉🏾 {}
+ * *boolean 👉🏾 true/false
+ */
+
+/** //* Math Operations
+ * *Mutiply *
+ * *Divide /
+ * *Exponents **
+ * *Modulo/Remainder % 5%2 = 1
+ * *Add +
+ * *Subtract -
+ */
+
+/** //* Math.Methods
+ * *Floor-Rounds Up
+ * *Ceil-Rounds Down
+ * *Random- give you a random number-----Math.random()*3___get number between 3
+ */
