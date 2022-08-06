e5316df64e4ab4f9c7efbd7589d70d98b340bf77
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2022-08-06 05:50:47 -0400

    Working on DOM manipulation

--- DOM-Manipulation/domscript.js
+++ DOM-Manipulation/domscript.js
@@ -1,13 +1,1 @@
-console.log("hello");
 
-let titleDiv = document.getElementById("title");
-
-console.log("before: ", title.innerText);
-
-let message = "Goodbye my lover";
-
-title.innerText = message;
-
-console.log("after: ", title.innerText);
-
-title.innerHTML = `<p>${message}</p>`;
--- dom/myscript.js
+++ dom/myscript.js
@@ -1,1 +1,15 @@
 // * DOM MANIPULATION
+
+console.log("hello");
+
+let titleDiv = document.getElementById("title");
+
+console.log("before: ", title.innerText);
+
+let message = "Goodbye my lover";
+
+title.innerText = message;
+
+console.log("after: ", title.innerText);
+
+title.innerHTML = `<p>${message}</p>`;
